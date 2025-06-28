da49ad86a5efca91a08f2b3083ffe19c5b0ca967
Angel Coeffier <acoeffier07@gmail.com>
2025-06-28 14:01:56 -0300

    Finalizando variables

---
+++ Clase 2/variables.py
@@ -0,0 +1,33 @@
+'''
+Lorem ipsum dolor sit amet, consectetur adipiscing elit.
+Sed do eiusmod tempor incididunt ut labore et dolore magna aliqua.
+Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat.
+Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur
+Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+'''
+
+print("Hola mundo desde la Unida")
+
+entero= 10
+flotante = 10.5
+cadena = "Hola mundo"
+booleano = True
+print(entero, flotante, cadena, booleano)
+print(type(entero), type(flotante), type(cadena), type(booleano))
+
+print("Sumando dos numeros")
+
+#Operaciones basicas con variables
+#Suma, resta, multiplicacion, division
+
+a = 10
+b = 20
+
+print("Suma de dos numeros:")
+print("Suma:", a + b)
+print("Resta de dos numeros:")
+print("Resta:", a - b)
+print("Multiplicacion de dos numeros:")
+print("Multiplicacion:", a * b)
+print("Division de dos numeros:")
+print("Division:", a / b)
